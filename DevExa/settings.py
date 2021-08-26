731fe60927e82ca86641c38f632d3d90ee42f55c
ferchin <ferhogo007@gmail.com>
2021-08-25 21:15:48 -0600

    changes modles

--- DevExa/settings.py
+++ DevExa/settings.py
@@ -72,17 +72,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DevExa.wsgi.application'
 
+#Database
+#https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'FerchDB',
+        'USER': 'ferchindb',
+        'PASSWORD': '1346',
+       'HOST': '127.0.0.1',
+       'PORT': 5432
     }
 }
-"""
+
+""""
 import dj_database_url
 from decouple import config
 
@@ -91,6 +96,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- apps/web/models.py
+++ apps/web/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Comidas(models.Model):
-    pk_comidas = models.AutoField(primary_key=True)
-    menus = models.CharField(max_length=50, blank=False, null=False)
-    precio = models.CharField(max_length=50, blank=False, null=False)
-    nombre = models.CharField(max_length=50, blank=False, null=False)
-    description = models.TextField(blank=False, null=True)
+class product(models.Model):
+    pk_product = models.AutoField(primary_key=True, null=False, blank=False)
+    code = models.CharField(max_length=9, null=False, blank=False)
+    name = models.CharField(max_length= 50, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(null=False, blank=False, max_digits=2, decimal_places=2)
+
+class stock(models.Model):
+    pk_stock = models.AutoField(primary_key=True, null=False, blank=False)
+    amount = models.IntegerField(null=False, blank=False)
+    fk_product = models.OneToOneField(product, null=False, blank=False, on_delete=models.CASCADE)
+
+class tallas(models.Model):
+    pk_tallas = models.AutoField(primary_key=True, null=False, blank=False)
+    adultos = models.CharField(max_length=80, null=False, blank=False)
+    niños = models.CharField(max_length=80, null=False, blank=False)
+
+class marcas(models.Model):
+    pk_marcas = models.AutoField(primary_key=True, null=False, blank=False)
+    nike = models.CharField(max_length=80, null=False, blank=False)
+    addidas = models.CharField(max_length=80, null=False, blank=False)
+    puma = models.CharField(max_length=80, null=False, blank=False)
+    converse = models.CharField(max_length=80, null=False, blank=False)
+    
+
+
